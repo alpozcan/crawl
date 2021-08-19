64cc7b7f6ac1a97d625e9743cf626dfd9acfb0aa
Edgar A. Bering IV <trizor@gmail.com>
2021-08-19 20:20:30 +0300

    feat: new monster, putrid mouth
    Putrid mouths are a new Tar monster, meant to be a more substatial (ha)
    wraith genus monster to fill out the branch flavour set of threats. They
    are based on Chinese mythology around hungry ghosts and re-use the old
    hungry ghost tile since it was so cool. As a monster they aren't the
    toughest of Tar threat, but the mix of miasma, poison, and warning cry
    should combine with the small levels to prevent a player who mismanaged
    the encounter from recovering before reenforcements arrive.

--- crawl-ref/source/monster-type.h
+++ crawl-ref/source/monster-type.h
@@ -745,6 +745,7 @@ enum monster_type                      // env.mons[].type
     MONS_LOST_SOUL,
     MONS_JIANGSHI,
     MONS_SEARING_WRETCH,
+    MONS_PUTRID_MOUTH,
 #endif
     MONS_SKELETON_SMALL,   // recolouring only
     MONS_SKELETON_LARGE,   // recolouring only
@@ -1125,6 +1126,7 @@ enum monster_type                      // env.mons[].type
     MONS_SEARING_WRETCH,
     MONS_QUICKSILVER_ELEMENTAL,
     MONS_CRYSTAL_ECHIDNA,
+    MONS_PUTRID_MOUTH,
 #endif
 
     NUM_MONSTERS,               // used for polymorph
